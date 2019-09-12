c353f862e3fe938f01b0797977c8cf0ff0ed0ab9
Pedro Silva <pedrompsilva@users.noreply.github.com>
2019-09-12 12:22:07 +0100

    latest version

--- run_analysis.R
+++ run_analysis.R
@@ -1,63 +1,76 @@
+  library(dplyr)
+  library(data.table)
+  #library(tidyselect)
+  
+  
+  
 
+  #read activity labels
+    labels <- read.csv(".\\UCI HAR Dataset\\activity_labels.TXT", col.names = c("activityId","activityDesc"), sep=" ", header = FALSE)
+  
+  #prepare varibles names & identify variables and columns to read from data sets
+    features <- read.csv(".\\UCI HAR Dataset\\features.TXT", 
+                         sep = " ", 
+                         header = FALSE, 
+                         col.names = c("idx", "feature"), 
+                         strip.white = TRUE)
+    varIndex <- grep("mean|std", features$feature)
+    varNames <- gsub("[^a-z0-9]","",tolower(features[varIndex, 2]))
+    
+    a <- cbind(varIndex, varNames)
 
-#include
-
-library(data.table)
-library(dplyr)
-
-
-#common loads
-colHeaders <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/features.txt")
-activities <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/activity_labels.txt")
-names(activities) <- c("act_id", "activity")
-
-
-# building train set
-trainSubject <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/train/subject_train.txt")
-trainX <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/train/X_train.txt")
-trainY <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/train/Y_train.txt")
-
-#add column names
-names(trainSubject)<-"subject"
-names(trainX) <- t(colHeaders[2])
-names(trainY) <- "act_id"
-
-#joining all train dsets 
-trainDS <- cbind(trainSubject, trainY, trainX)
-
-# handle test sets
-testSubject <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/test/subject_test.txt")
-testX <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/test/X_test.txt")
-testY <- read.table("./Coursera - Getting Data Project/final project/UCI HAR Dataset/test/Y_test.txt")
-
-#add column names
-names(testSubject)<-"subject"
-names(testX) <- t(colHeaders[2])
-names(testY) <- "act_id"
-
-#joining all test dsets 
-testDS <- cbind(testSubject, testY, testX)
-
-#build final DSet
-totalDS <- rbind(trainDS, testDS) 
-totalDS <- left_join(totalDS,activities)
-totalDS <- select(totalDS,subject, activity, matches("-mean[()]|-std[()]"))
-
-
-#cleanup
-rm(trainDS, testDS, activities)
-rm(trainX, trainY, trainSubject)
-rm(testX,  testY, testSubject, colHeaders)
-
-
-# perform aggregation
-group_subject_activity <- group_by(totalDS, subject, activity)
-agg <- sapply(names(group_subject_activity)[3:68] ,function(x) substitute(mean(x), list(x=as.name(x))))
-agg_subject_activity <- do.call(summarise, c(list(.data=group_subject_activity), agg))
-
-#cleanup
-rm(group_subject_activity, agg, totalDS)
-
-agg_subject_activity
-
-print("End of script")
+  #read train data set
+  trainSetData <- fread(file=".\\UCI HAR Dataset\\train\\X_train.TXT",
+                        col.names = varNames,
+                        select = varIndex,
+                        header = FALSE,
+                        data.table = FALSE)
+  trainSubjects <- read.csv(".\\UCI HAR Dataset\\train\\subject_train.TXT" ,header=FALSE, col.names = "subject")
+  trainActivities <- read.csv(".\\UCI HAR Dataset\\train\\y_train.TXT", col.names = "activityId", header = FALSE)
+  
+  
+  trainActivities <- left_join(trainActivities, labels)
+  trainSetData <- cbind("subject" = trainSubjects, "activity" = trainActivities$activityDesc, trainSetData)
+  
+  
+  
+  #read test data set=====================================================================
+  
+  testSetData <- fread(file=".\\UCI HAR Dataset\\test\\X_test.TXT",
+                        col.names = varNames,
+                        select = varIndex,
+                        header = FALSE,
+                        data.table = FALSE)
+  
+  testSubjects <- read.csv(".\\UCI HAR Dataset\\test\\subject_test.TXT" ,header=FALSE, col.names = "subject")
+  testActivities <- read.csv(".\\UCI HAR Dataset\\test\\y_test.TXT", col.names = "activityId", header = FALSE)
+  
+  #assemble test data set
+  testActivities <- left_join(testActivities, labels)
+  testSetData <- cbind("subject" = testSubjects, "activity" = testActivities$activityDesc, testSetData)
+  
+  
+  #merge test & train sets
+  finalDataSet <- rbind(testSetData, trainSetData)
+  finalDataSet <- arrange (dataSet, subject, activity)
+  
+  
+  #mean aggregatation per subject and activity
+  summaryDataSet <- aggregate(finalDataSet[3:81],by=list(subject = finalDataSet$subject, activity = finalDataSet$activity) , mean)
+  summaryDataSet <- arrange(summaryDataSet, subject, activity)
+  
+  
+  #clear memory
+  rm(testSetData, 
+     testSubjects, 
+     testActivities,
+     trainSetData, 
+     trainSubjects, 
+     trainActivities,
+     dataSet, 
+     varNames, 
+     varIndex, 
+     features, 
+     labels)  
+  
+  
