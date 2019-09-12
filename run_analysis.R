318b8a81988ae79d01ab55a79eee2bbc7ff31ea4
Pedro Silva <pedrompsilva@users.noreply.github.com>
2019-09-12 13:56:51 +0100

    added file output and mem cleanup
    writes the output data sets to file
    cleans up unused objects from memory

--- run_analysis.R
+++ run_analysis.R
@@ -1,76 +1,81 @@
+#======================================
+# Developed by Pedro Silva 
+# SEP 12 2019
+#======================================
+
+
   library(dplyr)
   library(data.table)
-  #library(tidyselect)
   
   
+#=========================================
+#   BLOCK 1 - preparing metadata
+#=========================================
   
-
-  #read activity labels
-    labels <- read.csv(".\\UCI HAR Dataset\\activity_labels.TXT", col.names = c("activityId","activityDesc"), sep=" ", header = FALSE)
-  
-  #prepare varibles names & identify variables and columns to read from data sets
-    features <- read.csv(".\\UCI HAR Dataset\\features.TXT", 
-                         sep = " ", 
-                         header = FALSE, 
-                         col.names = c("idx", "feature"), 
-                         strip.white = TRUE)
-    varIndex <- grep("mean|std", features$feature)
-    varNames <- gsub("[^a-z0-9]","",tolower(features[varIndex, 2]))
+    #read activity labels
+      labels <- read.csv(".\\UCI HAR Dataset\\activity_labels.TXT", col.names = c("activityId","activityDesc"), sep=" ", header = FALSE)
     
-    a <- cbind(varIndex, varNames)
+    #prepare varibles names & identify variables and columns to read from data sets
+      features <- read.csv(".\\UCI HAR Dataset\\features.TXT", 
+                           sep = " ", 
+                           header = FALSE, 
+                           col.names = c("idx", "feature"), 
+                           strip.white = TRUE)
+      varIndex <- grep(".*\\-mean\\(\\)-.*|.*\\-std\\(\\)-.*", features$feature)
+      varNames <- gsub("[^a-z0-9]","",tolower(features[varIndex, 2]))
+  
+      rm(features)  
+#=========================================          
+#   BLOCK 2 - uploading data sets 
+#=========================================        
+      
+      
+    #read and assemble train data set
+    trainData <- fread(file=".\\UCI HAR Dataset\\train\\X_train.TXT",
+                          col.names = varNames,
+                          select = varIndex,
+                          header = FALSE,
+                          data.table = FALSE)
+    trainSubjects <- read.csv(".\\UCI HAR Dataset\\train\\subject_train.TXT" ,header=FALSE, col.names = "subject")
+    trainActivities <- read.csv(".\\UCI HAR Dataset\\train\\y_train.TXT", col.names = "activityId", header = FALSE)
+    trainActivities <- left_join(trainActivities, labels, by="activityId")
 
-  #read train data set
-  trainSetData <- fread(file=".\\UCI HAR Dataset\\train\\X_train.TXT",
-                        col.names = varNames,
-                        select = varIndex,
-                        header = FALSE,
-                        data.table = FALSE)
-  trainSubjects <- read.csv(".\\UCI HAR Dataset\\train\\subject_train.TXT" ,header=FALSE, col.names = "subject")
-  trainActivities <- read.csv(".\\UCI HAR Dataset\\train\\y_train.TXT", col.names = "activityId", header = FALSE)
+    trainData <- cbind("subject" = trainSubjects, "activity" = trainActivities$activityDesc, trainData)
+    
   
+    #read and assemble test data set
+    testData <- fread(file=".\\UCI HAR Dataset\\test\\X_test.TXT",
+                          col.names = varNames,
+                          select = varIndex,
+                          header = FALSE,
+                          data.table = FALSE)
+    
+    testSubjects <- read.csv(".\\UCI HAR Dataset\\test\\subject_test.TXT" ,header=FALSE, col.names = "subject")
+    testActivities <- read.csv(".\\UCI HAR Dataset\\test\\y_test.TXT", col.names = "activityId", header = FALSE)
+    testActivities <- left_join(testActivities, labels, by="activityId")
   
-  trainActivities <- left_join(trainActivities, labels)
-  trainSetData <- cbind("subject" = trainSubjects, "activity" = trainActivities$activityDesc, trainSetData)
-  
-  
-  
-  #read test data set=====================================================================
-  
-  testSetData <- fread(file=".\\UCI HAR Dataset\\test\\X_test.TXT",
-                        col.names = varNames,
-                        select = varIndex,
-                        header = FALSE,
-                        data.table = FALSE)
-  
-  testSubjects <- read.csv(".\\UCI HAR Dataset\\test\\subject_test.TXT" ,header=FALSE, col.names = "subject")
-  testActivities <- read.csv(".\\UCI HAR Dataset\\test\\y_test.TXT", col.names = "activityId", header = FALSE)
-  
-  #assemble test data set
-  testActivities <- left_join(testActivities, labels)
-  testSetData <- cbind("subject" = testSubjects, "activity" = testActivities$activityDesc, testSetData)
-  
-  
-  #merge test & train sets
-  finalDataSet <- rbind(testSetData, trainSetData)
-  finalDataSet <- arrange (dataSet, subject, activity)
-  
-  
-  #mean aggregatation per subject and activity
-  summaryDataSet <- aggregate(finalDataSet[3:81],by=list(subject = finalDataSet$subject, activity = finalDataSet$activity) , mean)
-  summaryDataSet <- arrange(summaryDataSet, subject, activity)
-  
-  
-  #clear memory
-  rm(testSetData, 
-     testSubjects, 
-     testActivities,
-     trainSetData, 
-     trainSubjects, 
-     trainActivities,
-     dataSet, 
-     varNames, 
-     varIndex, 
-     features, 
-     labels)  
-  
+    testData <- cbind("subject" = testSubjects, "activity" = testActivities$activityDesc, testData)
+
+    #cleanup
+      rm(testSubjects, testActivities, trainSubjects, trainActivities, labels, varIndex, varNames)      
+    
+#=========================================        
+# BLOCK 3 assembling the final datasets 
+#=========================================    
+
+    #merge test & train sets
+      finalDataSet <- rbind(testData, trainData)
+      finalDataSet <- arrange (finalDataSet, subject, activity)
+    
+    
+    #mean aggregatation per subject and activity
+      summaryDataSet <- aggregate(finalDataSet[-c(1,2)],by=list(subject = finalDataSet$subject, activity = finalDataSet$activity) , mean)
+      summaryDataSet <- arrange(summaryDataSet, subject, activity)
+    
+    #cleanup
+      rm(testData, trainData)
+    
+    # write to file
+      write.csv(finalDataSet, file="finalDataSet.csv")
+      write.csv(summaryDataSet, file="summaryDataSet.csv")
   
